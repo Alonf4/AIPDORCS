02f4908a6c166a2833ee9e4ddcd764131f894a5b
Alonf4 <alonf4@gmail.com>
2022-06-09 21:09:01 +0300

    PyTorch Intro: Creating Model

--- GNN/PyTorch Introduction.py
+++ GNN/PyTorch Introduction.py
@@ -1,5 +1,6 @@
 # %% ANCHOR Import libraries
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
@@ -59,3 +60,29 @@ for X, y in test_data_loader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
+# %% ANCHOR Creating a learning model
+# Get cpu or gpu device for training
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} for training")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+# Creating an instance of NeuralNetwork, and move it to the device
+model = NeuralNetwork().to(device)
+print(model)
