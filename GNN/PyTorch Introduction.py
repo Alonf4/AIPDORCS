1593c4cf2a0f0e1b7e76c8feed223313cb3f3d09
Alonf4 <alonf4@gmail.com>
2022-06-09 20:19:48 +0300

    PyTorch Intro: Visualizing and batching data

--- GNN/PyTorch Introduction.py
+++ GNN/PyTorch Introduction.py
@@ -1,12 +1,11 @@
-# %% ANCHOR
+# %% ANCHOR Import libraries
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
-# %% ANCHOR
-# Download training data from open datasets
+# %% ANCHOR Download training data from open datasets
 training_data = datasets.FashionMNIST(
     root      = "data",
     train     = True,
@@ -22,14 +21,40 @@ test_data = datasets.FashionMNIST(
     transform = ToTensor()
 )
 
-# %% ANCHOR
+# %% ANCHOR Visualizing data
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot"
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows  = 3, 3
+for i in range(1, cols * rows + 1): 
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+# %% ANCHOR Batching data
+# For reshuffling the data at every epoch to reduce model overfitting, 
+# and use Python's multiprocessing to speed up data retrieval.
 batch_size = 64
 
 # Create data loaders
-train_data_loader = DataLoader(training_data, batch_size=batch_size)
-test_data_loader  = DataLoader(test_data, batch_size=batch_size)
+train_data_loader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+test_data_loader  = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-for X, y in test_data_loader:
+for X, y in test_data_loader: 
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
