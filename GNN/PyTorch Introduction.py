27097374232d0cb85b4d98dd3dddca1cb2a41b22
Alonf4 <alonf4@gmail.com>
2022-06-09 19:31:48 +0300

    Data Load

--- GNN/PyTorch Introduction.py
+++ GNN/PyTorch Introduction.py
@@ -34,29 +34,3 @@ for X, y in test_data_loader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
-# %% ANCHOR
-# Get cpu or gpu device for training
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-# Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512), 
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
-    
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-    
-model = NeuralNetwork().to(device)
-print(model)
-
