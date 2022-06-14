31da5e1f3cb3baef934aa91cdcac162e3a215d96
Alonf4 <alonf4@gmail.com>
2022-06-14 18:25:52 +0300

    PyTorch Intro: Training and testing model

--- GNN/PyTorch Introduction.py
+++ GNN/PyTorch Introduction.py
@@ -112,3 +112,26 @@ def train(dataloader, model, loss_fn, optimizer):
         loss, current = loss.item(), batch * len(X)
         print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+# Checking model's performance against the test set
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, current = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg Loss: {test_loss:>8f} \n")
+
+# Training and testing the model
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_data_loader, model, loss_fn, optimizer)
+    test(test_data_loader, model, loss_fn)
+print("Done!")
