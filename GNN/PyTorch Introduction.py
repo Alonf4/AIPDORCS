8b2bf6c5317edd7045017d03fd9a3fe7fe681675
Alonf4 <alonf4@gmail.com>
2022-06-14 18:15:45 +0300

    PyTorch Intro: Training model

--- GNN/PyTorch Introduction.py
+++ GNN/PyTorch Introduction.py
@@ -86,3 +86,29 @@ class NeuralNetwork(nn.Module):
 # Creating an instance of NeuralNetwork, and move it to the device
 model = NeuralNetwork().to(device)
 print(model)
+
+# %% ANCHOR Optimizing the model parameters
+# Initializing loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+# Training function
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+    
+    # Compute prediction error
+    pred = model(X)
+    loss = loss_fn(pred, y)
+    
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if batch % 100 == 0:
+        loss, current = loss.item(), batch * len(X)
+        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
