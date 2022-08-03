b9cee7525768bc9f5e4d631135f368f0ef8ea6c7
Alonf4 <alonf4@gmail.com>
2022-08-03 20:09:56 +0300

    Added more walls to survey

--- Website/EngineersChallenge.js
+++ Website/EngineersChallenge.js
@@ -7,44 +7,45 @@ let randomActivate = true;
 
 $w.onReady(async function () {
    // Scrolling to the top of the screen after refreshing:
-   wixWindow.scrollTo(0, 0, {"scrollAnimation": false});
+   wixWindow.scrollTo(0, 0, { "scrollAnimation": false });
 
    // Picking a random project and showing its data:
    $w("#randomDataset").onReady(async () => {
-   if(randomActivate);
-   console.log("Activate random dataset.");
-   let database = $w('#randomDataset').getTotalCount();  //This line gets the total count
-   let mixon    = Math.floor(Math.random() * database);  //This line begins randomizing code
-   $w("#randomDataset").setCurrentItemIndex(mixon)
-   .then(() => {
-      //You can use this section to do something else after the code finishes randomizing
-      console.log("Randomizing is complete.");
-      let file               = $w("#randomDataset").getCurrentItem();
-      let image              = file.floorScheme;                       //This line gets the current image
-      $w ("#popUpImage").src = image;                                  //This line labels our button
-      $w ('#bigImage').src   = image;
-      let title              = file.title;
-      });
+      if (randomActivate);
+      console.log("Activate random dataset.");
+      let database = $w('#randomDataset').getTotalCount(); //This line gets the total count
+      let mixon = Math.floor(Math.random() * database); //This line begins randomizing code
+      $w("#randomDataset").setCurrentItemIndex(mixon)
+         .then(() => {
+            //You can use this section to do something else after the code finishes randomizing
+            console.log("Randomizing is complete.");
+            let file = $w("#randomDataset").getCurrentItem();
+            let image = file.floorScheme; //This line gets the current image
+            $w("#popUpImage").src = image; //This line labels our button
+            $w('#bigImage').src = image;
+            let title = file.title;
+         });
    });
 });
 // A help function for randomizing the dataset of projects:
 function random(items) {
- var settings = items.length, randomize, index;
- while (0 !== settings) {
-          index      = Math.floor(Math.random() * settings);
-          settings  -= 1;
-          randomize  = items[settings];
-    items[settings]  = items[index];
-    items[settings]  = randomize;
-  }
- return items;
+   var settings = items.length,
+      randomize, index;
+   while (0 !== settings) {
+      index = Math.floor(Math.random() * settings);
+      settings -= 1;
+      randomize = items[settings];
+      items[settings] = items[index];
+      items[settings] = randomize;
+   }
+   return items;
 }
 // ----------------------------------------------------------------------------------------------------------
 // Showing a red arrow on the position of the pop-up control:
-export function text66_mouseIn(event) {
+export function hoverText_mouseIn(event) {
    $w('#arrow').expand();
 }
-export function text66_mouseOut(event) {
+export function hoverText_mouseOut(event) {
    $w('#arrow').collapse();
 }
 
@@ -64,8 +65,8 @@ export function minimizeButton_click(event) {
 // ----------------------------------------------------------------------------------------------------------
 // Loading the Project ID automatically:
 export function inputProjectID_click(event) {
-   $w('#inputProjectID').value                 = $w('#text65').text;
-   $w('#inputProjectID').style.borderColor     = "#E3E3E3";
+   $w('#inputProjectID').value = $w('#ProjectID').text;
+   $w('#inputProjectID').style.borderColor = "#E3E3E3";
    $w('#inputProjectID').style.backgroundColor = "#DED8D3";
 }
 
@@ -77,44 +78,44 @@ export function inputProjectID_click(event) {
 function ID(i) {
    let elements = [
       [
-      [$w('#categoryChoice1')], 
-      [$w('#beamText1'), $w('#beamSelection1')], 
-      [$w('#columnText1'), $w('#columnSelection1')], 
-      [$w('#wallText1'), $w('#wallSelection1')], 
-      [$w('#slabText1'), $w('#slabSelection1')], 
-      [$w('#elementComments1'), $w('#sliderText1'), $w('#slider1')]
-      ], 
+         [$w('#categoryChoice1')],
+         [$w('#beamText1'), $w('#beamSelection1')],
+         [$w('#columnText1'), $w('#columnSelection1')],
+         [$w('#wallText1'), $w('#wallSelection1')],
+         [$w('#slabText1'), $w('#slabSelection1')],
+         [$w('#elementComments1'), $w('#sliderText1'), $w('#slider1')]
+      ],
       [
-      [$w('#categoryChoice2')], 
-      [$w('#beamText2'), $w('#beamSelection2')], 
-      [$w('#columnText2'), $w('#columnSelection2')], 
-      [$w('#wallText2'), $w('#wallSelection2')], 
-      [$w('#slabText2'), $w('#slabSelection2')], 
-      [$w('#elementComments2'), $w('#sliderText2'), $w('#slider2')]
-      ], 
+         [$w('#categoryChoice2')],
+         [$w('#beamText2'), $w('#beamSelection2')],
+         [$w('#columnText2'), $w('#columnSelection2')],
+         [$w('#wallText2'), $w('#wallSelection2')],
+         [$w('#slabText2'), $w('#slabSelection2')],
+         [$w('#elementComments2'), $w('#sliderText2'), $w('#slider2')]
+      ],
       [
-      [$w('#categoryChoice3')], 
-      [$w('#beamText3'), $w('#beamSelection3')], 
-      [$w('#columnText3'), $w('#columnSelection3')], 
-      [$w('#wallText3'), $w('#wallSelection3')], 
-      [$w('#slabText3'), $w('#slabSelection3')], 
-      [$w('#elementComments3'), $w('#sliderText3'), $w('#slider3')]
-      ], 
+         [$w('#categoryChoice3')],
+         [$w('#beamText3'), $w('#beamSelection3')],
+         [$w('#columnText3'), $w('#columnSelection3')],
+         [$w('#wallText3'), $w('#wallSelection3')],
+         [$w('#slabText3'), $w('#slabSelection3')],
+         [$w('#elementComments3'), $w('#sliderText3'), $w('#slider3')]
+      ],
       [
-      [$w('#categoryChoice4')], 
-      [$w('#beamText4'), $w('#beamSelection4')], 
-      [$w('#columnText4'), $w('#columnSelection4')], 
-      [$w('#wallText4'), $w('#wallSelection4')], 
-      [$w('#slabText4'), $w('#slabSelection4')], 
-      [$w('#elementComments4'), $w('#sliderText4'), $w('#slider4')]
-      ], 
+         [$w('#categoryChoice4')],
+         [$w('#beamText4'), $w('#beamSelection4')],
+         [$w('#columnText4'), $w('#columnSelection4')],
+         [$w('#wallText4'), $w('#wallSelection4')],
+         [$w('#slabText4'), $w('#slabSelection4')],
+         [$w('#elementComments4'), $w('#sliderText4'), $w('#slider4')]
+      ],
       [
-      [$w('#categoryChoice5')], 
-      [$w('#beamText5'), $w('#beamSelection5')], 
-      [$w('#columnText5'), $w('#columnSelection5')], 
-      [$w('#wallText5'), $w('#wallSelection5')], 
-      [$w('#slabText5'), $w('#slabSelection5')], 
-      [$w('#elementComments5'), $w('#sliderText5'), $w('#slider5')]
+         [$w('#categoryChoice5')],
+         [$w('#beamText5'), $w('#beamSelection5')],
+         [$w('#columnText5'), $w('#columnSelection5')],
+         [$w('#wallText5'), $w('#wallSelection5')],
+         [$w('#slabText5'), $w('#slabSelection5')],
+         [$w('#elementComments5'), $w('#sliderText5'), $w('#slider5')]
       ]
    ];
    return elements[i];
@@ -133,7 +134,7 @@ function expandCategory(i, j) {
 
 // A help function for collapsing all categories but j category:
 function collapseCategory(i, j) {
-   for (var category = 1 ; category <= 4 ; category++) {
+   for (var category = 1; category <= 4; category++) {
       if (category != j) {
          ID(i)[category][0].collapse();
          ID(i)[category][1].selectedIndices = [];
@@ -154,26 +155,26 @@ function collapseCategory(i, j) {
 // A help function for expanding and collapsing fields based on category of elements chosen:
 function switchCollapseExpand(i) {
    switch (ID(i)[0][0].value) {
-      case 'None': 
+      case 'None':
          collapseCategory(i, 0);
          break;
-      case 'Beams': 
+      case 'Beams':
          collapseCategory(i, 1);
          expandCategory(i, 1);
          break;
-      case 'Columns': 
+      case 'Columns':
          collapseCategory(i, 2);
          expandCategory(i, 2);
          break;
-      case 'Walls': 
+      case 'Walls':
          collapseCategory(i, 3);
          expandCategory(i, 3);
          break;
-      case 'Slabs': 
+      case 'Slabs':
          collapseCategory(i, 4);
          expandCategory(i, 4);
          break;
-      default: 
+      default:
          break;
    }
 }
@@ -197,114 +198,117 @@ export function categoryChoice5_change(event) {
 
 // ----------------------------------------------------------------------------------------------------------
 // Selecting only "All elements" and removing everyone else
-let beams = 100; let columns = 50; let walls = 100; let slabs = 25;
+let beams = 100;
+let columns = 50;
+let walls = 125;
+let slabs = 25;
 
 // Question 1:
 export function beamSelection1_change(event) {
-	if ($w('#beamSelection1').selectedIndices.includes(beams)) {
+   if ($w('#beamSelection1').selectedIndices.includes(beams)) {
       $w('#beamSelection1').selectedIndices = [beams];
    }
 }
 export function columnSelection1_change(event) {
-	if ($w('#columnSelection1').selectedIndices.includes(columns)) {
+   if ($w('#columnSelection1').selectedIndices.includes(columns)) {
       $w('#columnSelection1').selectedIndices = [columns];
    }
 }
 export function wallSelection1_change(event) {
-	if ($w('#wallSelection1').selectedIndices.includes(walls)) {
+   if ($w('#wallSelection1').selectedIndices.includes(walls)) {
       $w('#wallSelection1').selectedIndices = [walls];
    }
 }
 export function slabSelection1_change(event) {
-	if ($w('#slabSelection1').selectedIndices.includes(slabs)) {
+   if ($w('#slabSelection1').selectedIndices.includes(slabs)) {
       $w('#slabSelection1').selectedIndices = [slabs];
    }
 }
 
 // Question 2:
 export function beamSelection2_change(event) {
-	if ($w('#beamSelection2').selectedIndices.includes(beams)) {
+   if ($w('#beamSelection2').selectedIndices.includes(beams)) {
       $w('#beamSelection2').selectedIndices = [beams];
    }
 }
 export function columnSelection2_change(event) {
-	if ($w('#columnSelection2').selectedIndices.includes(columns)) {
+   if ($w('#columnSelection2').selectedIndices.includes(columns)) {
       $w('#columnSelection2').selectedIndices = [columns];
    }
 }
 export function wallSelection2_change(event) {
-	if ($w('#wallSelection2').selectedIndices.includes(walls)) {
+   if ($w('#wallSelection2').selectedIndices.includes(walls)) {
       $w('#wallSelection2').selectedIndices = [walls];
    }
 }
 export function slabSelection2_change(event) {
-	if ($w('#slabSelection2').selectedIndices.includes(slabs)) {
+   if ($w('#slabSelection2').selectedIndices.includes(slabs)) {
       $w('#slabSelection2').selectedIndices = [slabs];
    }
 }
 
 // Question 3:
 export function beamSelection3_change(event) {
-	if ($w('#beamSelection3').selectedIndices.includes(beams)) {
+   if ($w('#beamSelection3').selectedIndices.includes(beams)) {
       $w('#beamSelection3').selectedIndices = [beams];
    }
 }
 export function columnSelection3_change(event) {
-	if ($w('#columnSelection3').selectedIndices.includes(columns)) {
+   if ($w('#columnSelection3').selectedIndices.includes(columns)) {
       $w('#columnSelection3').selectedIndices = [columns];
    }
 }
 export function wallSelection3_change(event) {
-	if ($w('#wallSelection3').selectedIndices.includes(walls)) {
+   if ($w('#wallSelection3').selectedIndices.includes(walls)) {
       $w('#wallSelection3').selectedIndices = [walls];
    }
 }
 export function slabSelection3_change(event) {
-	if ($w('#slabSelection3').selectedIndices.includes(slabs)) {
+   if ($w('#slabSelection3').selectedIndices.includes(slabs)) {
       $w('#slabSelection3').selectedIndices = [slabs];
    }
 }
 
 // Question 4:
 export function beamSelection4_change(event) {
-	if ($w('#beamSelection4').selectedIndices.includes(beams)) {
+   if ($w('#beamSelection4').selectedIndices.includes(beams)) {
       $w('#beamSelection4').selectedIndices = [beams];
    }
 }
 export function columnSelection4_change(event) {
-	if ($w('#columnSelection4').selectedIndices.includes(columns)) {
+   if ($w('#columnSelection4').selectedIndices.includes(columns)) {
       $w('#columnSelection4').selectedIndices = [columns];
    }
 }
 export function wallSelection4_change(event) {
-	if ($w('#wallSelection4').selectedIndices.includes(walls)) {
+   if ($w('#wallSelection4').selectedIndices.includes(walls)) {
       $w('#wallSelection4').selectedIndices = [walls];
    }
 }
 export function slabSelection4_change(event) {
-	if ($w('#slabSelection4').selectedIndices.includes(slabs)) {
+   if ($w('#slabSelection4').selectedIndices.includes(slabs)) {
       $w('#slabSelection4').selectedIndices = [slabs];
    }
 }
 
 // Question 5:
 export function beamSelection5_change(event) {
-	if ($w('#beamSelection5').selectedIndices.includes(beams)) {
+   if ($w('#beamSelection5').selectedIndices.includes(beams)) {
       $w('#beamSelection5').selectedIndices = [beams];
    }
 }
 export function columnSelection5_change(event) {
-	if ($w('#columnSelection5').selectedIndices.includes(columns)) {
+   if ($w('#columnSelection5').selectedIndices.includes(columns)) {
       $w('#columnSelection5').selectedIndices = [columns];
    }
 }
 export function wallSelection5_change(event) {
-	if ($w('#wallSelection5').selectedIndices.includes(walls)) {
+   if ($w('#wallSelection5').selectedIndices.includes(walls)) {
       $w('#wallSelection5').selectedIndices = [walls];
    }
 }
 export function slabSelection5_change(event) {
-	if ($w('#slabSelection5').selectedIndices.includes(slabs)) {
+   if ($w('#slabSelection5').selectedIndices.includes(slabs)) {
       $w('#slabSelection5').selectedIndices = [slabs];
    }
 }
@@ -328,7 +332,7 @@ export function generalComments_change(event) {
 // ----------------------------------------------------------------------------------------------------------
 // Expanding/collapsing question 7.2 based on further comments in question 7.1
 export function additionalComments_change(event) {
-	if ($w('#additionalComments').value != "") {
+   if ($w('#additionalComments').value != "") {
       $w('#sliderText7').expand();
       $w('#slider7').expand();
    } else {
@@ -340,17 +344,18 @@ export function additionalComments_change(event) {
 // ----------------------------------------------------------------------------------------------------------
 // Calculating the overall score based on sliders when clicking "Calculate"
 export function calculate_click(event) {
-	$w('#overallScore').value = 100 - ($w('#slider1').value + $w('#slider2').value + $w('#slider3').value + 
-   $w('#slider4').value + $w('#slider5').value + $w('#slider6').value + $w('#slider7').value);
+   let CalculatedScore = 100 - ($w('#slider1').value + $w('#slider2').value + $w('#slider3').value +
+      $w('#slider4').value + $w('#slider5').value + $w('#slider6').value + $w('#slider7').value);
+   $w('#overallScore').value = CalculatedScore;
 }
 
 // ----------------------------------------------------------------------------------------------------------
 // Submit button actions:
 export function submitButton_click(event) {
-   $w('#inputProjectID').value                 = "";
-   $w('#inputProjectID').style.borderColor     = "#FF4040";
+   $w('#inputProjectID').value = "";
+   $w('#inputProjectID').style.borderColor = "#FF4040";
    $w('#inputProjectID').style.backgroundColor = "#FFFFFF";
-   setTimeout(()=>{
-      wixWindow.scrollTo(90, 280, {"scrollAnimation": true});
-   },1500);
+   setTimeout(() => {
+      wixWindow.scrollTo(90, 280, { "scrollAnimation": true });
+   }, 1500);
 }
