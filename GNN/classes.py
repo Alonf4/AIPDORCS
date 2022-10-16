6933c88a5ccb0d0cbc310a1884379014d8964918
Alon D. Argaman <alonf4@gmail.com>
2022-10-16 23:59:22 +0300

    AIPDORCS Classes Update

--- GNN/classes.py
+++ GNN/classes.py
@@ -1,11 +1,12 @@
-# ------------------------- Structural Element Class ------------------------- #
+# ---------------------------------------------------------------------------- #
+#                           Structural Element Class                           #
+# ---------------------------------------------------------------------------- #
 class Element:
     """A class of a structural element, containing an element id, type, all its features and all connections to other structural elements.
     
     Class Attributes
     ----------------
         ``featuresDict (dict)``: A dictionary with keys of element types and values of number of features.
-        ``countDict (dict)``: A dictionary containing the number of elements for each type, and the total existing number of elements.
     
     Instance Attributes
     -------------------
@@ -16,12 +17,12 @@ class Element:
     
     Class Methods
     -------------
-    ``homoFeatureCount()``: Returns the smallest number of features of all types, for a uniform number of features.
-    ``totalCount()``: Returns the number of existing elements overall.
+        ``homoFeatureCount()``: Returns the smallest number of features of all types, for a uniform number of features.
+        ``totalCount()``: Returns the number of existing elements overall.
     
     Instance Methods
     ----------------
-    ``typeCount()``: Returns the number of existing elements of this type.
+        ``typeCount()``: Returns the number of existing elements of this type.
     
     Examples
     --------
@@ -35,9 +36,10 @@ class Element:
     >>> # Output: '[B1],[1, 2, 3],[]'
     """
     featuresDict = {}
-    countDict = {'Total': 0}
+    _countDict = {'Total': 0}
     
-    def __init__(self, id:str, features:list=None, connections:list=None):
+    # ---------------------------- Constructor Method ---------------------------- #
+    def __init__(self, id:str, features:list=None, connections:list[str]=None):
         self.id = id
         
         # Initialize element type with None in case of empty dictionary or a mismatch between the dictionary and the id:
@@ -61,27 +63,29 @@ class Element:
             self.connections = []
         
         # If this is the first instance of this type, initialize the counter for this type:
-        if not self.type in Element.countDict.keys():
-            Element.countDict[self.type] = 0
+        if not self.type in Element._countDict.keys():
+            Element._countDict[self.type] = 0
         # Increase the total number of elements for each new element instance:
-        Element.countDict['Total'] += 1
-        Element.countDict[self.type] += 1
+        Element._countDict['Total'] += 1
+        Element._countDict[self.type] += 1
     
+    # ----------------------------- Destructor Method ---------------------------- #
     def __del__(self):
         # Decrease the total number of elements for each deleted element instance:
-        Element.countDict['Total'] -= 1
-        Element.countDict[self.type] -= 1
+        Element._countDict['Total'] -= 1
+        Element._countDict[self.type] -= 1
     
     def __str__(self) -> str:
         return f'[{self.id}],{self.features},{self.connections}'
     
+    # ---------------- Get Number of Features for Homogenous Graph --------------- #
     @classmethod
-    def homoFeatureCount(cls):
+    def homoFeatureCount(cls) -> int:
         """Based on the class attribute ``featuresDict (dict)``, return the smallest number of features.
         
         Returns
         -------
-        ``count (int)``: The smallest number of features an element type can have.
+        ``(int)``: The smallest number of features an element type can have.
         
         Examples
         --------
@@ -105,27 +109,73 @@ class Element:
         
         return count
     
+    # ------------------- Get Total Number of Elements Existing ------------------ #
     @classmethod
     def totalCount(cls):
-        return cls.countDict['Total']
+        return cls._countDict['Total']
     
+    # ----------------- Get the Number of Elements from this Type ---------------- #
     def typeCount(self):
-        return Element.countDict[self.type]
+        return Element._countDict[self.type]
 
 class Node:
+    """A class of a graph nodes, containing a node id and a structural element.
+    
+    Instance Attributes
+    -------------------
+        ``nodeID (int)``: The id of the graph node.
+        ``element (Element)``: A structural element class instance.
+    
+    Instance Methods
+    ----------------
+        ``getNodeAsList()``: Returns the node as a list for writing as a row in a CSV file.
+    """
+    # ---------------------------- Constructor Method ---------------------------- #
     def __init__(self, nodeID:int, element:Element):
         self.nodeID = nodeID
         self.element = element
     
+    # ---------------- Converting Node to List for Nodes.csv File ---------------- #
     def getNodeAsList(self):
         featureCount = self.element.homoFeatureCount()
         return [self.nodeID] + [self.element.id] + self.element.features[:featureCount]
 
 class Edge:
+    """A class of a graph edge, containing an edge id, a source node and a destination node.
+    
+    Instance Attributes
+    -------------------
+        ``edgeID (int)``: The id of the graph edge.
+        ``src (Node)``: A graph node class instance as the source of the edge.
+        ``dst (Node)``: A graph node class instance as the destination of the edge.
+    
+    Instance Methods
+    ----------------
+        ``getEdgeAsList()``: Returns the edge as a list for writing as a row in a CSV file.
+    
+    Examples
+    --------
+    >>> # Checking if two edges contain the same graph nodes in any direction:
+    >>> edge1 = Edge(0, node1, node2)
+    >>> edge2 = Edge(1, node2, node1)
+    >>> print(e1 == e2)
+    >>> # Output: 'True'
+    >>> # This test can also be helpful in finding an edge in a list:
+    >>> eList = [edge1]
+    >>> print(edge2 in eList)
+    >>> # Output: 'True'
+    """
+    # ---------------------------- Constructor Method ---------------------------- #
     def __init__(self, edgeID:int, src:Node, dst:Node):
         self.edgeID = edgeID
         self.src = src
         self.dst = dst
     
+    # ----------------- Check if Two Edges Contain the same Nodes ---------------- #
+    def __eq__(self, other) -> bool:
+        return ((self.src.element.id == other.src.element.id) and (self.dst.element.id == other.dst.element.id)) \
+            or ((self.src.element.id == other.dst.element.id) and (self.dst.element.id == other.src.element.id))
+    
+    # ---------------- Converting Edge to List for Edges.csv File ---------------- #
     def getEdgeAsList(self):
         return [self.src.nodeID] + [self.dst.nodeID]
