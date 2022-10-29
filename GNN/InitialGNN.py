cb06a4fe22e3096350e37a54939731b1c2b742f5
Alon D. Argaman <alonf4@gmail.com>
2022-10-30 00:54:09 +0300

    Initial Graph Classification Model

---
+++ GNN/InitialGNN.py
@@ -0,0 +1,90 @@
+# ------------------------------ Modules Import ------------------------------ #
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from torch.utils.data import DataLoader
+from dgl.dataloading import GraphDataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from dgl.nn import GraphConv
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GraphConv(in_feats, h_feats)
+        self.conv2 = GraphConv(h_feats, num_classes)
+
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.relu(h)
+        h = self.conv2(g, h)
+        g.ndata['h'] = h
+        return dgl.mean_nodes(g, 'h')
+
+def main():
+    workspace = os.getcwd()
+    datasetDir = f'{workspace}\\Database\\dataset.bin'
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    # Hyper parameters:
+    epochs = 200
+    batch_size = 1
+    n_classes = 2
+    lr = 0.005
+    
+    # Getting the graphs from our database:
+    dataset = dgl.load_graphs(datasetDir)[0]
+    
+    num_examples = len(dataset)
+    num_train = int(num_examples * 0.8)
+    
+    # data split
+    train_sampler = SubsetRandomSampler(torch.arange(num_train))
+    test_sampler = SubsetRandomSampler(torch.arange(num_train, num_examples))
+    
+    # data batch for parallel computation
+    train_dataloader = GraphDataLoader(dataset, sampler=train_sampler, batch_size=batch_size, drop_last=False)
+    test_dataloader = GraphDataLoader(dataset, sampler=test_sampler, batch_size=batch_size, drop_last=False)
+    
+    # Output for testing the training dataset:
+    it = iter(train_dataloader)
+    batch = next(it)
+    print(batch)
+    
+    batched_graph = batch
+    print('Number of nodes for each graph element in the batch:', batched_graph.batch_num_nodes())
+    print('Number of edges for each graph element in the batch:', batched_graph.batch_num_edges())
+
+    # Recover the original graph elements from the minibatch
+    graphs = dgl.unbatch(batched_graph)
+    print('The original graphs in the minibatch:')
+    print(graphs)
+    
+    # Create the model with given dimensions
+    model = GCN(dataset.dim_nfeats, 16, dataset.gclasses)
+    # FIXME - Need to add node features first
+    #FIXME - Need to add labels first
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+    for epoch in range(20):
+        for batched_graph, labels in train_dataloader:
+            pred = model(batched_graph, batched_graph.ndata['attr'].float())
+            loss = F.cross_entropy(pred, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    num_correct = 0
+    num_tests = 0
+    for batched_graph, labels in test_dataloader:
+        pred = model(batched_graph, batched_graph.ndata['attr'].float())
+        num_correct += (pred.argmax(1) == labels).sum().item()
+        num_tests += len(labels)
+
+    print('Test accuracy:', num_correct / num_tests)
+
+# ------------------------------- Run as Script ------------------------------ #
+if __name__ == '__main__':
+    main()
